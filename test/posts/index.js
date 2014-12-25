d46e57a31a6f91cd5a73565b7f4e9e2aa2c76313
freedonaab <freedonaab@gmail.com>
2014-12-25 21:09:40 +0100

    added tests for "when getting a post/a list or posts, dont retrieve all fields from db like a pig" feature

--- test/posts/index.js
+++ test/posts/index.js
@@ -168,4 +168,106 @@ describe('/posts', function () {
         });
     });
 
+    it('GET a post should not retrieve all fields', function (done) {
+        async.waterfall([
+            testUtils.createUser(config, {
+                email: "mouloud1@hotmail.fr",
+                username: "kebab94",
+                password: "wallah123"
+            }),
+            function (res, next) {
+                var userId = res.body.data.user._id;
+                testUtils.createPostWithErrorChecking(config, {
+                    title: "Watch this adorable little puppy die in fire",
+                    content: "http://127.0.0.1:8080/fake/images/success/123456.gif",
+                    author_id: userId
+                })(next);
+            },
+            function (post, next) {
+                testUtils.getPostWithErrorChecking(config, post._id)(next);
+            }
+        ], function (err, post) {
+            should.not.exist(err);
+            should.exist(post);
+            post.should.have.property("title", "Watch this adorable little puppy die in fire");
+            post.should.have.property("content", "http://127.0.0.1:8080/fake/images/success/123456.gif");
+            post.should.have.property("content_type", "image");
+            post.should.have.property('author_id');
+            post.should.have.property('status');
+            post.should.have.property('votes');
+            post.votes.should.have.property('hotness');
+            post.votes.should.have.property('score');
+            post.votes.should.not.have.property('ups');
+            post.votes.should.not.have.property('downs');
+            post.votes.score.should.have.property('down');
+            post.votes.score.should.have.property('up');
+            post.votes.score.should.have.property('total');
+            post.should.have.property('comments');
+            post.should.have.property('updated_at');
+            post.should.have.property('created_at');
+            done();
+        });
+    });
+
+    it('when retrieving a list of posts, not all fields should be present', function (done) {
+        async.waterfall([
+            testUtils.createUser(config, {
+                email: "mouloud1@hotmail.fr",
+                username: "kebab94",
+                password: "wallah123"
+            }),
+            function (res, next) {
+                var userId = res.body.data.user._id;
+                testUtils.createPosts(config, [{
+                    title: "Watch this adorable little puppy die in fire",
+                    content: "http://127.0.0.1:8080/fake/images/success/123456.jpg",
+                    author_id: userId
+                },{
+                    title: "Found this in my toilets this morning",
+                    content: "http://127.0.0.1:8080/fake/images/success/1544646.jpg",
+                    author_id: userId
+                },{
+                    title: "You will never guess how this little cat did that",
+                    content: "http://127.0.0.1:8080/fake/images/success/56465.jpg",
+                    author_id: userId
+                }])(next);
+            },
+            function (postIds, next) {
+                testUtils.get(config, '/posts')(next);
+            }
+        ], function (err, res) {
+            should.not.exist(err);
+            should.exist(res);
+            res.should.have.property('body');
+            res.body.should.have.property('metadata');
+            res.body.should.have.property('data');
+            res.body.metadata.should.have.property('success', true);
+            res.body.metadata.should.have.property('error', null);
+            res.body.metadata.should.have.property('statusCode', 200);
+            res.body.data.should.have.property('posts');
+            res.body.data.posts.should.be.an.Array;
+            res.body.data.posts.should.have.length(3);
+            for (var i = 0; i < res.body.data.posts.length; ++i) {
+                res.body.data.posts[i].should.have.property('title');
+                res.body.data.posts[i].should.have.property('content');
+                res.body.data.posts[i].should.have.property('content_type');
+                res.body.data.posts[i].should.have.property('author_id');
+                res.body.data.posts[i].should.have.property('status');
+                res.body.data.posts[i].should.have.property('votes');
+                res.body.data.posts[i].votes.should.have.property('hotness');
+                res.body.data.posts[i].votes.should.have.property('score');
+                res.body.data.posts[i].votes.should.not.have.property('ups');
+                res.body.data.posts[i].votes.should.not.have.property('downs');
+                res.body.data.posts[i].votes.score.should.have.property('down');
+                res.body.data.posts[i].votes.score.should.have.property('up');
+                res.body.data.posts[i].votes.score.should.have.property('total');
+                res.body.data.posts[i].should.not.have.property('comments');
+                res.body.data.posts[i].should.have.property('updated_at');
+                res.body.data.posts[i].should.have.property('created_at');
+            }
+            done();
+        });
+    });
+
+
 });
